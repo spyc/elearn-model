c040b48397fb281cbc09d4ab42cf39051a424bc2
Tony Yip <tony@opensource.hk>
2015-08-12 21:04:41 +0800

    Add Auth Function

--- src/User.php
+++ src/User.php
@@ -63,4 +63,82 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
         parent::__construct($attributes);
         $this->setKeyName('pycid');
     }
+
+    /**
+     * Check User has Permission
+     *
+     * @param string $permission Permission Name.
+     *
+     * @return bool Auth or not.
+     */
+    public function hasPermission($permission)
+    {
+        $permission = Permission::select('id')
+            ->where(['name' => $permission])
+            ->first();
+        $id = $permission->id;
+        $auth = DB::table('user_auth')
+            ->select(['active'])
+            ->where([
+                'pycid' => $this->pycid,
+                'permission' => $id
+            ])->first();
+        return null !== $auth && $auth->active;
+    }
+
+    /**
+     * Get post in the community.
+     *
+     * @param string $community
+     *
+     * @return string post or null
+     */
+    public function getCommunityPost($community)
+    {
+        $community = Community::select('id')
+            ->where(['name' => $community])
+            ->first();
+
+        $community = $community->id;
+
+        $committee = Committee::select('post')
+            ->where([
+                'pycid' => $this->pycid,
+                'community' => $community
+            ])
+            ->first();
+
+        if (null === $committee) {
+            return null;
+        }
+
+        return $committee->post;
+    }
+
+    public function hasCommunityAuth($community, $permission)
+    {
+        $post = $this->getCommunityPost($community);
+
+        $community = Community::select('id')
+            ->where(['name' => $community])
+            ->first();
+
+        $community = $community->id;
+
+        $permission = Permission::select('id')
+            ->where(['name' => $permission])
+            ->first();
+        $id = $permission->id;
+
+        $auth = DB::table('community_permission')
+            ->select('active')
+            ->where([
+                'community' => $community,
+                'post' => $post,
+                'permission' => $id
+            ])
+            ->first();
+
+        return null !== $auth && $auth->active;
+    }
 }
